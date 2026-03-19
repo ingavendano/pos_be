d02b5187c4053a222326290b9f0f6b45732a30a8
ingavendano <carlosavendanoing@gmail.com>
2026-03-19 12:36:27 -0600

    Actualizacion Migracion

--- src/main/resources/db/migration/V14__add_position_to_tables.sql
+++ src/main/resources/db/migration/V14__add_position_to_tables.sql
@@ -1,3 +1,3 @@
 -- Migration to add position coordinates to tables for the interactive map
-ALTER TABLE tables ADD COLUMN posx INTEGER DEFAULT 0 NOT NULL;
-ALTER TABLE tables ADD COLUMN posy INTEGER DEFAULT 0 NOT NULL;
+ALTER TABLE restaurant_tables ADD COLUMN posx INTEGER DEFAULT 0 NOT NULL;
+ALTER TABLE restaurant_tables ADD COLUMN posy INTEGER DEFAULT 0 NOT NULL;
